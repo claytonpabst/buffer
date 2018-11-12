86409fd545a20f8ed6d0ae7477bb7c73ebd30e8a
Clayton Pabst <claytonpabst@gmail.com>
2018-11-12 13:22:54 -0700

    multer added

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const massive = require('massive');
-const app = module.exports = express();
 const config = require('./config.js');
+const multer = require('multer');
+
+const app = module.exports = express();
+let upload  = multer({ storage: multer.memoryStorage() });
 
 app.use(bodyParser.json());
 
@@ -13,7 +16,25 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + './../build'))
 
-const userController = require("./userController.js");
+app.post('/test', upload.single('asset'), function(req, res) {  
+  req.on('data', function(chunk){
+    console.log(chunk)
+  })
+  req.on('end', function(){
+    console.log(req.buffer)
+    console.log(req.body);
+    console.log(req.file);
+    console.log(req.files);
+    res.send();
+  })
+});
+
+// app.post('/test', upload.array('asset'), (req, res) => {  
+//   console.log(req.body)
+//   console.log(req.file);
+//   console.log(req.files);
+//   res.send();
+// });
 
 //////////Endpoints for the front end
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './Home.css';
 
 
 class Home extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      assetToUpload: null,
+    }
+  }
+
+  postAssetToServer = () => {
+    console.log(document.getElementById("assetInput").files[0])
+    axios.post('/test', {asset:document.getElementById("assetInput").files[0]}).then(res => {
+      console.log(res)
+    }).catch(err => {
+      alert("Could not process asset upload at this time.")
+    })
+  }
 
   render() {
     return (
-      <div className="home">
+      <div onClick={this.test} className="home">
 
-          Home Page
+        <input id="assetInput" name="asset" type="file" style={{width:"100%", height:"100%", opacity:"1"}}/>
+        <button onClick={this.postAssetToServer} style={{}}>Upload</button>
+
+        <form action="/test" method="post" encrypt="multipart/form-data">  
+          <input type="file" name="asset" />
+          <input type="submit" />
+        </form> 
+
 
       </div>
     );
